0f0240e7ad87bf13850f614a7454aaf9557a5130
adibfiroz <adibfiroz9702@gmail.com>
2026-01-18 20:56:06 +0530

    add google login and more logics added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import ClientOnly from '@/components/ClientOnly'
 import HomeClient from '@/components/home-client'
 import prismadb from '@/lib/prisma'
+import getCurrentUser from './actions/getCurrentUser';
 
 export const dynamic = 'force-dynamic';
 
 const Home = async () => {
+
+  const currentUser = await getCurrentUser();
+
   const trips = await prismadb.trip.findMany({
+    where: {
+      userId: currentUser?.id
+    },
+    include: {
+      expenses: true
+    },
     orderBy: {
       createdAt: 'desc',
     },
@@ -14,7 +24,7 @@ const Home = async () => {
   return (
     <div>
       <ClientOnly>
-        <HomeClient trips={trips} />
+        <HomeClient currentUser={currentUser} trips={trips} />
       </ClientOnly>
     </div>
   )
